4e226d1b730f0475deacf05e55003a30cb9d8aba
91805 <hritik.raheja_cs19@gla.ac.in>
2020-09-17 13:50:25 +0530

    Added constructor for Department.java.

--- src/definitions/Department.java
+++ src/definitions/Department.java
@@ -4,4 +4,8 @@ public class Department {
     private String departmentName;
     private Employee[] allEmployeesInDepartment = new Employee[10];
     private int numberOfEmployees = 0;
+
+    public Department(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
