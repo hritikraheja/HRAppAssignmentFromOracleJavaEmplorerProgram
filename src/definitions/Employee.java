7581d64f8929f8eeb248c124e589214ea337535f
91805 <hritik.raheja_cs19@gla.ac.in>
2020-09-17 13:45:34 +0530

    Created constructors in Employee.java.

--- src/definitions/Employee.java
+++ src/definitions/Employee.java
@@ -4,4 +4,19 @@ public class Employee {
     private int identificationNumber;
     private String name;
     private double salary;
+
+    public Employee(int identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public Employee(int identificationNumber, String name) {
+        this.identificationNumber = identificationNumber;
+        this.name = name;
+    }
+
+    public Employee(int identificationNumber, String name, double salary) {
+        this.identificationNumber = identificationNumber;
+        this.name = name;
+        this.salary = salary;
+    }
 }
