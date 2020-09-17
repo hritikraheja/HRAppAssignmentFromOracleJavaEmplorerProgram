9358b710ddde5b3459fd77aa0e4aba25a258cf22
91805 <hritik.raheja_cs19@gla.ac.in>
2020-09-17 13:49:31 +0530

    Created all necessary fields in Department.java.

---
+++ src/definitions/Department.java
@@ -0,0 +1,7 @@
+package definitions;
+
+public class Department {
+    private String departmentName;
+    private Employee[] allEmployeesInDepartment = new Employee[10];
+    private int numberOfEmployees = 0;
+}
